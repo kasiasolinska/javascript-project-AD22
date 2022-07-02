da2c002ff57a5f612692368936fd059fea63afa0
kasiasolinska <catherine.bus@gmail.com>
2022-07-02 20:23:21 +0200

    Tasks + local storage

--- scripts/mainScreen.js
+++ scripts/mainScreen.js
@@ -4,6 +4,12 @@ const $countryInput = document.getElementById("country_input");
 const $generateButton = document.getElementById("generate_id");
 const $toggleButton = document.getElementById("toggle_screen");
 
+const $toggleScreens = document.getElementById("toggle_screens");
+const $addTask = document.getElementById("add_task");
+const $taskList = document.getElementById("task_list");
+const $newTaskInput = document.getElementById("new_task_input");
+const $loadTasks = document.getElementById("load_tasks");
+
 $toggleButton.addEventListener("click", () => {
   document.getElementById("general_screen").classList.toggle("hidden");
   document.getElementById("notes_screen").classList.toggle("hidden");
@@ -55,17 +61,63 @@ $generateButton.addEventListener("click", () => {
   fetch("https://randomuser.me/api/")
     .then((res) => res.json())
     .then((res) => {
+      const id = res.results[0];
+      if (id) {
+        const newName =
+          res.results[0].name.first + " " + res.results[0].name.last;
+        const email = res.results[0].email;
+        const avatar = res.results[0].picture.medium;
+
+        document.getElementById("new_email").innerText = email;
+        document.getElementById("new_name").innerText = newName;
+        document.getElementById("new_picture").src = avatar;
+      }
       console.log(res);
-
-      const newName =
-        res.results[0].name.first + " " + res.results[0].name.last;
-      const email = res.results[0].email;
-      const avatar = res.results[0].picture.medium;
-
-      document.getElementById("new_email").innerText = email;
-      document.getElementById("new_name").innerText = newName;
-      document.getElementById("new_picture").src = avatar;
     });
 });
+$addTask.addEventListener("click", () => {
+  const newTask = $newTaskInput.value;
+  $newTaskInput.value = "";
+  const listElement = document.createElement("li");
+  listElement.classList.add("task");
+  listElement.innerText = newTask;
+  $taskList.append(listElement);
+  const tasksFromLS = localStorage.getItem("tasks")
+    ? JSON.parse(localStorage.getItem("tasks"))
+    : [];
+  console.log(tasksFromLS);
+  tasksFromLS.push(newTask);
+  localStorage.setItem("tasks", JSON.stringify(tasksFromLS));
+});
+
+$taskList.addEventListener("click", (e) => {
+  if (e.target.classList.contains("task")) {
+    const taskToDelete = e.target.innerText;
+    e.target.remove();
+    const tasksFromLS = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
+      : [];
+    const tasksAfterDelete = tasksFromLS.filter(
+      (task) => task !== taskToDelete
+    );
+    localStorage.setItem("tasks", JSON.stringify(tasksAfterDelete));
+  }
+});
+
+$loadTasks.addEventListener("click", () => {
+  if ($taskList.childNodes.length === 0) {
+    const tasksFromLS = localStorage.getItem("tasks")
+      ? JSON.parse(localStorage.getItem("tasks"))
+      : [];
+
+    tasksFromLS.forEach((taskText) => {
+      const listElement = document.createElement("li");
+      listElement.classList.add("task");
+      listElement.innerText = taskText;
+      $taskList.append(listElement);
+    });
+  }
+});
+
 $logout.addEventListener("click", logout);
 $countryButton.addEventListener("click", searchCountry);
